acd58691a02c59fbc394b16d95e93af8e7274150
Mario Fliegner <mario.fliegner@aqipro.com>
2023-11-09 10:13:00 +0100

    Formatierung zur besseren Lesbarkeit

--- js/adventskalenderdaten.js
+++ js/adventskalenderdaten.js
@@ -1,26 +1,122 @@
 const dataObject = {
-    "1": { "image": "bilder/tag1.jpg", "text": "Freiwillige Feuerwehr Grünwald", "link": "https://feuerwehr-gruenwald.de" },
-    "2": { "image": "bilder/tag2.png", "text": "KI-supported Foto", "link": "https://openai.com/dall-e-3" },
-    "3": { "image": "bilder/tag3.png", "text": "KI-supported Foto", "link": "https://openai.com/dall-e-3" },
-    "4": { "image": "bilder/tag4.png", "text": "KI-supported Foto", "link": "https://openai.com/dall-e-3" },
-    "5": { "image": "bilder/tag5.png", "text": "KI-supported Foto", "link": "https://openai.com/dall-e-3" },
-    "6": { "image": "bilder/tag6.png", "text": "KI-supported Foto", "link": "https://openai.com/dall-e-3" },
-    "7": { "image": "https://picsum.photos/id/155/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "8": { "image": "https://picsum.photos/id/167/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "9": { "image": "https://picsum.photos/id/185/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "10": { "image": "https://picsum.photos/id/10/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "11": { "image": "https://picsum.photos/id/69/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "12": { "image": "https://picsum.photos/id/112/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "13": { "image": "https://picsum.photos/id/200/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "14": { "image": "https://picsum.photos/id/245/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "15": { "image": "https://picsum.photos/id/45/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "16": { "image": "https://picsum.photos/id/17/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "17": { "image": "https://picsum.photos/id/179/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "18": { "image": "https://picsum.photos/id/202/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "19": { "image": "https://picsum.photos/id/18/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "20": { "image": "https://picsum.photos/id/44/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "21": { "image": "https://picsum.photos/id/268/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "22": { "image": "https://picsum.photos/id/1/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "23": { "image": "https://picsum.photos/id/274/1024/768/?blur", "text": "Lorem Picsum (picsum.photos)", "link": "https://picsum.photos/" },
-    "24": { "image": "bilder/tag24.jpg", "text": "Frohe Weihnachten wünscht euch die Freiwillige Feuerwehr Grünwald!", "link": "https://www.feuerwehr-gruenwald.de/" }
-  };
+    "1": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/0c/e9/0ce9ff40-b033-48a0-af32-953504756c84/geraetehaus_von_oben.jpg", 
+        "text": "Freiwillige Feuerwehr Grünwald", 
+        "link": "https://feuerwehr-gruenwald.de" 
+    },
+    "2": { 
+        "image": "https://picsum.photos/id/12/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "3": { 
+        "image": "https://picsum.photos/id/35/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "4": { 
+        "image": "https://picsum.photos/id/120/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "5": { 
+        "image": "https://picsum.photos/id/64/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "6": { 
+        "image": "https://picsum.photos/id/8/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "7": { 
+        "image": "https://picsum.photos/id/155/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "8": { 
+        "image": "https://picsum.photos/id/167/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "9": { 
+        "image": "https://picsum.photos/id/185/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "10": { 
+        "image": "https://picsum.photos/id/10/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "11": { 
+        "image": "https://picsum.photos/id/69/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "12": { 
+        "image": "https://picsum.photos/id/112/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "13": { 
+        "image": "https://picsum.photos/id/200/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "14": { 
+        "image": "https://picsum.photos/id/212/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "15": { 
+        "image": "https://picsum.photos/id/45/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "16": { 
+        "image": "https://picsum.photos/id/17/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "17": { 
+        "image": "https://picsum.photos/id/179/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "18": { 
+        "image": "https://picsum.photos/id/18/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "19": { 
+        "image": "https://picsum.photos/id/91/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "20": { 
+        "image": "https://picsum.photos/id/180/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "21": { 
+        "image": "https://picsum.photos/id/321/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "22": { 
+        "image": "https://picsum.photos/id/322/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "23": { 
+        "image": "https://picsum.photos/id/323/1024/768/?blur", 
+        "text": "Lorem Picsum (picsum.photos)", 
+        "link": "https://picsum.photos/" 
+    },
+    "24": { 
+        "image": "https://picsum.photos/id/324/1024/768/?blur", 
+        "text": "<strong>Frohe Weihnachten</strong> wünscht euch die Freiwillige Feuerwehr Grünwald!", 
+        "link": "https://picsum.photos/" 
+    }
+};
