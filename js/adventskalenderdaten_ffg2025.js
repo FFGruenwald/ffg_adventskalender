fc9157eda30e59362f3732f4f4a2f85ea3843a6e
Mario Fliegner <mario.fliegner@aqipro.com>
2025-12-10 12:49:32 +0100

    Kalenderdaten für FFG Kalender 2025

---
+++ js/adventskalenderdaten_ffg2025.js
@@ -0,0 +1,170 @@
+const dataObject = {
+    "defaultVideoThumbnail": "bilder/videoplayer.png",
+    "defaultLinkText": "Weitere Informationen...",
+    "backgroundImage": {
+        "large": "https://www.feuerwehr-gruenwald.de/media/filer_public/ea/d4/ead4a4da-4eb8-4e19-86e5-5196b1c50ce0/kalenderbild.jpg",
+        "small":"https://www.feuerwehr-gruenwald.de/media/filer_public/ea/d4/ead4a4da-4eb8-4e19-86e5-5196b1c50ce0/kalenderbild.jpg"
+    },
+    "texte": {
+        "popupHeader_showPopupForPastDoors_false": "Türchen nicht mehr verfügbar",
+        "popupText_showPopupForPastDoors_false": "Leider bist du zu spät. Dieses Türchen ist nicht mehr verfügbar.",
+    },
+    "1": {  
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/fb/dc/fbdcc11a-3ef5-4d8c-a360-01ab562c0260/2024_08_13_ffg_geraetehaus_front_fahrzeuge.jpg", 
+        "text": "Unser 150-jähriges Jubiläumsjahr geht langsam zu Ende – und genau deshalb gibt’s für euch jetzt noch etwas zum Freuen: Bis Weihnachten könnt ihr hier <strong>jeden Tag ein Türchen</strong> unseres Adventskalenders öffnen. Und am <strong>5. Dezember</strong> öffnet sich zusätzlich ein echtes Türchen am Feuerwehrhaus – im Rahmen des Lebendigen Adventskalenders. Merkt euch das Datum und kommt vorbei – wir freuen uns auf euch.",
+        "link": "",
+        "linkText": ""
+    },
+    "2": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/29/b8/29b8e613-4fad-4262-9ef8-e69f1965fc21/2025_02.jpg", 
+        "text": "Funfact: Würden wir alle unsere verfügbaren B/C/D-Druckschläuche aneinanderkuppeln, kämen wir auf eine Strecke von rund <strong>5,2 Kilometern</strong>. Das entspricht einmal vom Parkplatz des Walderlebniszentrums bis rauf zur Bavariafilmstraße.", 
+        "link": "",
+        "linkText": ""
+    },
+    "3": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/c4/61/c4611f02-c459-4d2f-88df-8777bfbda22e/2025_03.jpg", 
+        "text": "<strong>Rückblick auf 150 Jahre:</strong> Am 26. März 2010 kam es im Einkaufszentrum „Schlosspassage“ zu einem der schwersten Brände in der Nachkriegsgeschichte unserer Gemeinde. Über 13 Stunden waren wir mit den Löscharbeiten beschäftigt.", 
+        "link": "https://www.feuerwehr-gruenwald.de/brand-schlosspassage/",
+        "linkText": "Rückblick zum Brand in der Schlosspassage"
+    },
+    "4": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/a9/2f/a92fc1cb-3360-455c-acdb-9c84315b6d41/2025_04.jpg", 
+        "text": "Fitness gehört bei uns dazu. Dieses Jahr haben mehrere unserer Mitglieder den Grünwalder Burglauf unter Atemschutz bewältigt – sowohl über 5 km als auch über 10 km. Der Applaus im Ziel war riesig. Nicht nur #WirFürGrünwald sondern auch #FitFürGrünwald", 
+        "link": "",
+        "linkText": ""
+    },
+    "5": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/cf/8b/cf8bc434-eca5-4519-88bd-2079c159dcc4/2025-12-05_-_lebendiger_adventskalender.jpg", 
+        "text": "Heute öffnen wir unser Türchen beim Lebendigen Adventskalender!<br/><strong>Wir freuen uns auf euch zwischen 17 Uhr und 19 Uhr bei uns am Feuerwehrhaus.</strong> Es wird weihnachtlich. Kommt vorbei und folgt den Spuren der Wichtel.", 
+        "link": "",
+        "linkText": ""
+    },
+    "6": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/01/77/01772684-4e52-40f4-a9b8-7b9d969a2158/2025_06.png",
+        "text": "Hydranten liefern unser Löschwasser – auch wenn man sie kaum sieht. In Grünwald gibt es 560 Stück, überwiegend Unterflurhydranten, erkennbar nur an einem solchen Schild. Bitte parken Sie nicht auf den Deckeln und halten Sie die Schilder im Winter frei – das spart im Ernstfall wertvolle Zeit.",
+        "link": "https://www.feuerwehr-gruenwald.de/unterflurhydrant/",
+        "linkText": "Video: Wir nehmen einen Unterflurhydrant in Betrieb"
+    },
+    "7": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/72/af/72af3818-ad82-44a6-be0f-7395e94002de/2025_07.jpg", 
+        "text": "Hier seht ihr den Arbeitsbereich unserer Atemschutzgerätewarte. Nach einem Brandeinsatz oder nach Übungen wird jedes eingesetzte Atemschutzgerät kontrolliert, gereinigt und wieder einsatzbereit gemacht.", 
+        "link": "https://www.feuerwehr-gruenwald.de/uber-uns/geraetehaus/#atemschutzwerkstatt",
+        "linkText": "360° Atemschutzwerkstatt"
+    },
+    "8": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/ce/54/ce54e852-e563-4710-827f-f3a23fa6cec2/2025_08.jpg", 
+        "text": "Unsere Feuerwehrfrauen freuen sich immer über Verstärkung – aktuell sind es 13. Wenn dir Technik, Teamwork und Verantwortung liegen: Bei uns bist du richtig.",
+        "link": "https://www.feuerwehr-gruenwald.de/mitmachen/",
+        "linkText": "Jetzt aktiv mitmachen!"
+    },
+    "9": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/54/5e/545e4834-7440-456d-b08c-0a5b9ec88716/2025_09.jpg", 
+        "text": "Alle Jahre wieder, kommt das Christuskind... - und manchmal leider auch die Feuerwehr! Die häufigsten Ursachen für Feuerwehreinsätze in dieser Zeit sind oft nachlässiger Umgang mit offenem Feuer.", 
+        "link": "https://www.feuerwehr-gruenwald.de/weihnachten",
+        "linkText": "Unsere Brandschutztipps zur Weihnachtszeit"
+    },
+    "10": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/36/32/3632b0a1-f905-49b0-a5d0-deaa6d5996dd/2025_10.jpg", 
+        "text": "<strong>Rückblick auf 150 Jahre:</strong> In der Nacht auf den 24. Juni 1991 zog ein schweres Gewitter über Grünwald. Vermutlich schlug ein Blitz in das Forsthaus Wörnbrunn ein, wodurch im Bereich der Rezeption ein Brand ausbrach.", 
+        "link": "https://www.feuerwehr-gruenwald.de/brand-woernbrunn/",
+        "linkText": "Mehr zum Brand im Forsthaus Wörnbrunn im Jahr 1991"
+    },
+    "11": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/7f/ad/7fad2b5a-4f08-4dd9-980e-90175514d698/2025_11.jpg", 
+        "text": "Rund 15-mal pro Jahr rücken wir auch zu Tierrettungen aus: oft zu Füchsen oder Mardern in Lichtschächten oder Schlangen in Häusern. Der Klassiker ‚Katze auf Baum‘ kommt bei uns tatsächlich nur ungefähr einmal pro Jahr vor.", 
+        "link": "",
+        "linkText": ""
+    },
+    "12": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/3a/d7/3ad7ed22-b1ba-4eb8-aef3-1707885ddb8d/2025_12.jpg", 
+        "text": "<strong>Rückblick auf 150 Jahre:</strong> Das Bild zeigt eine Übung unserer Wehr im Jahr 1964 am Marktplatz. Die damalige Steigleiter kam auf rund 14 Meter – also etwa bis ins 3. Obergeschoss. Unsere heutige Drehleiter erreicht 23 Meter Rettungshöhe bei 12 Metern Abstand zum Gebäude – damit schaffen wir bis zu sieben Stockwerke und decken auch Grünwalds höchste Wohnhäuser sicher ab.", 
+        "link": "https://www.feuerwehr-gruenwald.de/uber-uns/fahrzeuge/dlk/",
+        "linkText": "Mehr über unsere Drehleiter DLK 23/12 erfahren"
+    },
+    "13": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/85/68/8568b3bc-c8e3-441c-9234-6ac2fccfdc3c/2025_13.jpg", 
+        "text": "<strong>Rückblick auf 150 Jahre:</strong> Ja, auch bei den Olympischen Sommerspielen 1972 in München ging es ohne unsere Hilfe nicht... Mit Hilfe unserer Glocke konnte die letzte Runde des damaligen Straßenradrennens eingeleitet werden. Die Glocke steht heute als Erinnerungsstück bei uns in den Vereinsräumen.", 
+        "link": "https://www.gruenwalder-freizeitpark.de/media/pdf/Sport%20in%20Gr%C3%BCnwald%20-%20Glocke.pdf",
+        "linkText": "Anekdote zur Geschichte vom Grünwalder Freizeitpark"
+    },
+    "14": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/9c/c0/9cc03160-ed3b-4be2-b75b-456027983e58/2025_14.jpg", 
+        "text": "Seit über 60 Jahren verbindet uns mit der Freiwilligen Feuerwehr Steinhaus im Ahrntal eine echte Freundschaft – entstanden 1963 aus einer persönlichen Geschichte, gewachsen durch viele gemeinsame Erlebnisse und bis heute lebendig über Grenzen und Generationen hinweg.", 
+        "link": "",
+        "linkText": ""
+    },
+    "15": { 
+        "video": {
+            "source": "mp4",
+            "url": "https://www.feuerwehr-gruenwald.de/media/filer_public/d6/c2/d6c24a5d-43ab-4934-b3ec-a881a18f1b3e/2025_21.mp4",
+            "thumbnailImage": "https://www.feuerwehr-gruenwald.de/media/filer_public/91/23/9123ab84-383d-4d8e-a5ef-4cf297efdad2/2025_21.jpg",
+            "controls": true,
+            "width": "100%",
+            "height": 450
+        },
+        "text": "<strong>Rückblick: 150 Jahre Freiwillige Feuerwehr Grünwald - Ausstellung im Rathaus</strong></br>Der Aufbau unserer Jubiläumsausstellung im Rathaus am 29. April 2025 – festgehalten im Zeitraffer, inklusive abschließendem Rundgang.", 
+        "link": "",
+        "linkText": ""
+    },
+    "16": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/fa/39/fa39505a-85d2-4bf6-9d5f-3c9929f9c869/2025_16.jpg", 
+        "text": "Ohne Übung gibt’s im Einsatz keine Routine. Wir üben regelmäßig, so wie hier im Mai 2025 bei einer Zugübung unter dem Einsatzstichwort 'Tiefgaragenbrand'. ", 
+        "link": "",
+        "linkText": ""
+    },
+    "17": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/64/cf/64cf3bf9-5d41-4c84-b674-a7698d563ba9/2025_17.jpg", 
+        "text": "Seit 1982, als auf Initiative der Feuerwehren Ottobrunn und Planegg erstmals ein Skirennen für alle Feuerwehren des Landkreises München ins Leben gerufen wurde, sind wir mit dabei. Gleich beim allerersten Rennen ging der Wanderpokal zu uns nach Grünwald – und in den Jahrzehnten danach folgten unzählige weitere Topplatzierungen in der Mannschaftswertung. Auch seit der ersten Snowboardwertung, die 2007 eingeführt wurde, sind wir in dieser Kategorie mit dabei.", 
+        "link": "",
+        "linkText": ""
+    },
+    "18": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/9e/c3/9ec3c850-ec2a-466f-871f-8b236fa385cb/2025_18.jpg", 
+        "text": "<strong>Rückblick auf 150 Jahre:</strong> Das erste Tanklöschfahrzeug unserer Wehr – ein TLF 15/15 – wurde 1953 beschafft und war 19 Jahre in Grünwald im Einsatz. Danach ging es an die FF Börwang im Allgäu und blieb erhalten; heute befindet es sich dort im Privatbesitz. In unserem Jubiläumsjahr haben wir das Originalfahrzeug noch einmal „nach Hause“ geholt – ein wirklich besonderer Moment.", 
+        "link": "",
+        "linkText": ""
+    },
+    "19": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/c8/22/c8224dd8-5608-4bed-97d4-62fd3cf9c09c/2025_19.jpg", 
+        "text": "<strong>Rückblick auf 150 Jahre:</strong> Die ersten Feuerwehrhelme in Grünwald wurden am 15. Mai 1895 eingeführt – 15 Messinghelme, die damals einen echten Modernisierungsschub bedeuteten. Bis dahin waren Lederhelme üblich. Ein Originalhelm von 1895 hat die Zeit überdauert – und ist bis heute erhalten.", 
+        "link": "",
+        "linkText": ""
+    },
+    "20": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/42/23/422387a7-e0f3-4692-9c95-f0035860cccc/2025_20.jpg", 
+        "text": "<strong>Rückblick auf 150 Jahre:</strong> Das Gerätehaus unserer Wehr, aus dem die Feuerwehrmänner zwischen 1883 und 1959 ausrückten. Es stand in der Emil-Geis-Straße, Ecke Marktplatz in der Nähe der Verfassungslinde.", 
+        "link": "https://www.feuerwehr-gruenwald.de/geraetehaus-marktplatz/",
+        "linkText": "Vorher/Nachher Vergleich"
+    },
+    "21": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/14/27/142728f6-1948-4846-9b95-cd26e5efe689/2025_15.jpg", 
+        "text": "Brandschutzerziehung ist eine wichtige Aufgabe in unserem Verein. Seit 1954 vermitteln wir Kindern wichtiges Wissen rund um Feuer und Verhalten im Notfall. Ende November waren rund 116 Kinder der dritten Klassen der Martin-Kneidl-Grundschule im Rahmen des HSU-Unterrichts bei uns zu Besuch.", 
+        "link": "https://www.feuerwehr-gruenwald.de/brandschutzerziehung/",
+        "linkText": "Mehr zu unserer Brandschutzerziehung"
+    },
+    "22": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/5f/5b/5f5b4eb3-7b02-4720-9e79-804e7f9f3ff1/2025_22.jpg", 
+        "text": "Nicht jeder kann oder will bei der Feuerwehr aktiv mitmachen. Wer trotzdem etwas zur Sicherheit in unserer Gemeinde beitragen möchte, kann unseren Feuerwehrverein als Fördermitglied oder mit einer Spende unterstützen.", 
+        "link": "https://www.feuerwehr-gruenwald.de/foerdern/",
+        "linkText": "Über Fördermitgliedschaft oder Spendenmöglichkeit informieren"
+    },
+    "23": { 
+        "video": {
+            "source": "mp4",
+            "url": "https://www.feuerwehr-gruenwald.de/media/filer_public/69/2c/692cbdd4-d2f8-4d63-a5d6-31eb7caa7396/2025_23.mp4",
+            "thumbnailImage": "https://www.feuerwehr-gruenwald.de/media/filer_public/7f/c5/7fc5e9f0-fa9c-44a2-b406-bdf22af2842d/2025_23.jpg",
+            "controls": true,
+            "width": "100%",
+            "height": 450
+        },
+        "text": "<strong>Rückblick: 150 Jahre Freiwillige Feuerwehr Grünwald - Tag der offenen Tür</strong></br>Vielen Dank an die über 1000 Besucher unseres Tages der offenen Tür am 24. Mai 2025. Auch ihr habt unser Jubiläumsjahr mit zu einem besonderen gemacht. Dieses Video zeigt den Tag im Zeitraffer - inkl. Aufbau.", 
+        "link": "",
+        "linkText": ""
+    },
+    "24": { 
+        "image": "https://www.feuerwehr-gruenwald.de/media/filer_public/11/6a/116a492c-c690-4f24-9b5d-c32549d4d736/2025_24.jpg",
+        "text": "<strong>Genießt die Feiertage – bleibt gesund und kommt gut ins neue Jahr.</strong>",
+        "link": "",
+        "linkText": ""
+    }
+};
